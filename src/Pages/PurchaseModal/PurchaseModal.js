2d8f2d99a2744ce6129ff07a65e0cc1e7d771d8f
UhaiMong <uhaimarma2016@gmail.com>
2022-11-28 15:18:14 +0600

    code modified and update 4

--- src/Pages/PurchaseModal/PurchaseModal.js
+++ src/Pages/PurchaseModal/PurchaseModal.js
@@ -1,17 +1,132 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
+import { AuthContext } from '../../Context/Authprovider';
 
-const PurchaseModal = () => {
+const PurchaseModal = ({ productInfo }) => {
+    const { user } = useContext(AuthContext);
+    const { model, location, resalePrice, picture } = productInfo;
+    const navigate = useNavigate();
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const bookingHandler = (data) => {
+        const orders = {
+            phone: data.phone,
+            buyerName: data.name,
+            buyerLocation: data.myLocation,
+            seller: "admin",
+            productName: data.productName,
+            brand: data.productName,
+            price: data.price,
+            image: picture,
+            sellerEmail: data.email
+        }
+
+        fetch('http://localhost:8000/ordered', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(orders)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                alert('Successfully booked the product.');
+                navigate('/orderedProduct');
+            })
+    }
     return (
-        <div>
+        <>
             <input type="checkbox" id="purchase-modal" className="modal-toggle" />
             <div className="modal">
-                <div className="modal-box relative">
+                <div className="modal-box relative my-14 w-2/4 mx-auto bg-[#a8dadc] rounded-md">
                     <label htmlFor="purchase-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <h3 className="text-lg font-bold">{model}</h3>
+
+                    <form onSubmit={handleSubmit(bookingHandler)}>
+                        <div className='grid grid-cols-1 lg:grid-cols-2'>
+
+                            <div className="form-control w-full p-3">
+                                <label className='label-text'>Product name</label>
+                                <input className='input' defaultValue={model} readOnly type="text" {...register("productName")} />
+                            </div>
+
+                            <div className="form-control w-full p-3">
+                                <label className="label">
+                                    <span className="label-text">Seller Location</span>
+                                </label>
+                                <input className="input" type='text' defaultValue={location} readOnly
+                                    {...register('location')}>
+                                </input>
+                            </div>
+
+                            <div className="form-control w-full p-3">
+                                <label className="label">
+                                    <span className="label-text">Price</span>
+                                </label>
+
+                                <input className="input input-bordered w-full" type='text' defaultValue={resalePrice} readOnly {...register("price")}
+                                />
+                            </div>
+
+                            <div className="form-control w-full p-3">
+                                <label className="label">
+                                    <span className="label-text">Your Location</span>
+                                </label>
+
+                                <input className="input input-bordered w-full" type='text'
+                                    {...register("myLocation", { required: true })}
+                                    aria-invalid={errors.myLocation ? "true" : "false"}
+                                    placeholder="e.g: Dhaka, Segunbagicha"
+                                />
+                                {errors.myLocation?.type === 'required' && <p role="alert" className='text-red-700 font-xs'>Your location is required</p>}
+                            </div>
+
+                            <div className="form-control w-full p-3">
+                                <label className="label">
+                                    <span className="label-text">Your Name</span>
+                                </label>
+
+                                <input className="input input-bordered w-full" placeholder='Your name' type='text'
+                                    {...register("name", { required: true })}
+                                />
+                                {errors.name?.type === 'required' && <p role="alert" className='text-red-700 font-xs'>Your name is required</p>}
+                            </div>
+
+                            <div className="form-control w-full p-3">
+                                <label className="label">
+                                    <span className="label-text">Your Phone number</span>
+                                </label>
+
+                                <input className="input input-bordered w-full" type='text'
+                                    {...register("phone", { required: true })}
+                                    aria-invalid={errors.originalPrice ? "true" : "false"}
+                                    placeholder="e.g: 01234567890"
+                                />
+                                {errors.originalPrice?.type === 'required' && <p role="alert" className='text-red-700 font-xs'>Your Phone number is required</p>}
+                            </div>
+
+                            <div className="form-control w-full p-3">
+                                <label className="label">
+                                    <span className="label-text" defaultValue={user.email} readOnly>Your Email</span>
+                                </label>
+
+                                <input className="input input-bordered w-full" type='text'
+                                    {...register("email")}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-control my-3">
+                            <button className='btn btn-success text-white font-semibold capitalize'>Submit</button>
+                        </div>
+                    </form>
+
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/Pages/SelectedBrand/SelectedBrand.js
+++ src/Pages/SelectedBrand/SelectedBrand.js
@@ -1,65 +1,38 @@
-import React, { useContext } from 'react';
+import React, {useState } from 'react';
 import { useLoaderData } from 'react-router';
-import { AuthContext } from '../../Context/Authprovider';
+import PurchaseModal from '../PurchaseModal/PurchaseModal';
+import SelectedBrandById from './SelectedBrandById';
 
 const SelectedBrand = () => {
-    const {user} = useContext(AuthContext)
     const products = useLoaderData();
+    const [productInfo, setProductInfo] = useState(null);
     const elements = products.elements
     return (
         <div>
             <h1 className='text-2xl font-semibold'>You selected brand found {products.elements.length} products</h1>
             <h1 className='lg:text-3xl text-xl font-semibold'>Brand: {products.brand}</h1>
+
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
                     products &&
-                    elements?.map(product => <div className="bg-[#bde0fe] shadow-xl my-4 flex flex-col justify-between p-3 rounded-lg"
-                        key={product._id}
+                    elements.map(product => <SelectedBrandById
+                        key={product}
+                        product={product}
+                        setProductInfo={setProductInfo}
                     >
-
-                        <div className='rounded-md'>
-                            <img className='w-full h-52 rounded-md' src={product.picture} alt="Product" />
-                        </div>
-
-                        <div className="">
-                            <h2 className="text-xl font-semibold">{product.model}</h2>
-
-                            <div className='grid grid-cols-1 md:grid-cols-2 justify-between items-center'>
-                                <p>Location: {product.location}</p>
-                                <p className='line-through text-gray-500'>Tk: {product.originalPrice}</p>
-                                <p>Used: {product.usedTime}</p>
-                                <p className='font-semibold'>Tk: {product.resalePrice}</p>
-                                <p>Posted: {product.postedDate}</p>
-                                <div>
-                                    {
-                                        product.seller ?
-                                            <>
-                                                <button className="" title='Verified'>
-                                                    {product?.seller}
-                                                    <div className="badge badge-success"></div>
-                                                </button>
-                                            </>
-                                            :
-                                            <p>Unknown seller</p>
-                                    }
-                                </div>
-                            </div>
-
-                            <div className="card-actions justify-start mt-3">
-
-                                <button  className="btn btn-accent hover:btn-outline">WishList</button>
-
-                                <button
-                                >
-                                    <button className="btn btn-success hover:btn-outline">Purchase</button>
-                                </button>
-                                <label htmlFor="purchase-modal" className="btn btn-success hover:btn-outline">Purchase</label>
-                            </div>
-                        </div>
-
-                    </div>)
+                        
+                    </SelectedBrandById>)
                 }
+                
             </div>
+
+            {
+                productInfo &&
+                <PurchaseModal
+                productInfo={productInfo}
+            >
+
+            </PurchaseModal>}
         </div>
     );
 };
